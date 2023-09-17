92713e13adf0ba219fa072c73df819aa594acc82
Ethan B <bernhaen@mail.uc.edu>
2023-09-17 13:28:37 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+    int spring = 250;
+    int summer = 500;
+    int fall = 350;
+    int winter = 450;
+    int yearlyCost = 0;
+        System.out.println(" The Maintenance Cost of spring is " + spring);
+        System.out.println(" The Maintenance Cost of summer is " + summer);
+        System.out.println(" The Maintenance Cost of fall is " + fall);
+        System.out.println(" The Maintenance Cost of winter is " + winter);
+        yearlyCost = spring + summer + fall + winter;
+        System.out.println(" The Maintenance Cost of the whole year is  is " + yearlyCost );
+
+    }
+}
